0741ee8c10b5ea365f2916aa087d924365cf07a4
== <=>
2023-02-23 11:39:47 +0200

    Flutter-lesson20Extract widegt part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import  'quote.dart';
+import 'quote_card.dart';
 void main() {
   runApp(MaterialApp(
     home:QuoteList()
@@ -20,10 +21,7 @@ class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes =[Quote('ali', 'i love u'),
     Quote( 'ali','i love u salam maroun '),
     Quote('ali','i love u')];
-Widget quoteTemplate(quote){
-return  QuoteCard(quote :quote);
 
-}
    @override
    Widget build(BuildContext context) {
      return Scaffold(
@@ -47,7 +45,7 @@ return  QuoteCard(quote :quote);
 
          children: quotes.map((quote) {
 
-           return quoteTemplate(quote);
+           return QuoteCard(quote :quote);
      }
          ).toList(),
        )
@@ -55,62 +53,4 @@ return  QuoteCard(quote :quote);
    }
 }
 
-class QuoteCard extends StatelessWidget {
-final  Quote quote;
-  QuoteCard({required this.quote});
 
-  @override
-  Widget build(BuildContext context) {
-    return     Card(
-
-    margin: EdgeInsets.fromLTRB(16,16,16,0),
-
-      child: Padding(
-
-        padding: const EdgeInsets.all(12.0),
-
-        child: Column(
-
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-
-          children: <Widget>[
-
-            Text(
-
-              quote.text,
-
-              style: TextStyle(
-
-                fontSize: 18,
-
-                color:Colors.amber,
-
-              ),
-
-            ),
-
-            SizedBox(height: 6,),
-
-            Text(
-
-              quote.authors,
-
-              style: TextStyle(
-
-                fontSize: 14,
-
-                color:Colors.amber,
-
-              ),
-
-            )
-
-          ],
-
-        ),
-
-      ),
-
-    );
-  }
-}
---
+++ lib/quote_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import  'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  final  Quote quote;
+  QuoteCard({required this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return     Card(
+
+      margin: EdgeInsets.fromLTRB(16,16,16,0),
+
+      child: Padding(
+
+        padding: const EdgeInsets.all(12.0),
+
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: <Widget>[
+
+            Text(
+
+              quote.text,
+
+              style: TextStyle(
+
+                fontSize: 18,
+
+                color:Colors.amber,
+
+              ),
+
+            ),
+
+            SizedBox(height: 6,),
+
+            Text(
+
+              quote.authors,
+
+              style: TextStyle(
+
+                fontSize: 14,
+
+                color:Colors.amber,
+
+              ),
+
+            )
+
+          ],
+
+        ),
+
+      ),
+
+    );
+  }
+}
